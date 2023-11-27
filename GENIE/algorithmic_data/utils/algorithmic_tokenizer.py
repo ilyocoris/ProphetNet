ef0b6482aa544673161e1192fc268d9a24ad7795
Gustavo <gustavo.cilleruelo@gmail.com>
2023-11-27 17:21:01 +0000

    modulus sum

---
+++ GENIE/algorithmic_data/utils/algorithmic_tokenizer.py
@@ -0,0 +1,26 @@
+from transformers import PreTrainedTokenizer
+
+class AlgorithmicTokenizer(PreTrainedTokenizer):
+    def __init__(
+            self, 
+            vocab = ["[SEP]", "[CLS]", "[UNK]", "[PAD]", "+", "-", "*", "p", "q", "r", "s", "t", "m"] + [str(n) for n in list(range(0,113))], 
+            *args, 
+            **kwargs
+        ):
+        super().__init__(*args, **kwargs)
+
+        
+        self.vocab = {word.strip(): idx for idx, word in enumerate(vocab)}
+
+    def _tokenize(self, text):
+        # Implement your custom tokenization logic here
+        tokens = text.split()  # Replace this with your own tokenization logic
+        return tokens
+
+    def _convert_token_to_id(self, token):
+        # Convert a token to its corresponding ID in the vocabulary
+        return self.vocab.get(token, self.vocab.get(self.unk_token))
+
+    def _convert_id_to_token(self, index):
+        # Convert an ID to its corresponding token in the vocabulary
+        return list(self.vocab.keys())[index]
